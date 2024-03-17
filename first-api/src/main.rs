4dd86920cd65e5601d9d4e09b2f80880be0b6908
Lucas Freitas <lucasfrittas@gmail.com>
2024-03-17 00:22:42 -0300

    first implementation of a Rust API with a /hello endpoint example

---
+++ first-api/src/main.rs
@@ -0,0 +1,23 @@
+#![allow(unused)]
+
+use std::net::SocketAddr;
+
+use axum::{response::Html, Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().route(
+        "/hello", 
+        get(|| async { Html("Hello, <strong>World!!</strong>") }));
+
+
+    //region: ---Start Server
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("Listening on {addr}\n");
+    axum::Server::bind(&addr)
+        .serve(routes_hello.into_make_service())
+        .await
+        .unwrap()
+
+    //endregion ---Start Server
+}
